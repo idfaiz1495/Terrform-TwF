b41d2973b1637e570acf5fae69c1930928a04883
idfaiz1495 <143871882+idfaiz1495@users.noreply.github.com>
2023-11-11 09:21:13 +1100

    398

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,8 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "fslab-terraform-backend" 
-    key            = "twf/terraform.tfstate"
-    region         = "ap-southeast-2"
-    encrypt        = true
-  }
+terraform{
+    backend "s3" {
+        bucket = "fslab-terraform-backend"
+        key="twf/terraform.tfstate"
+        region="ap-southeast-2"
+        encrypt = true
+      
+    }
 }
