da0c0fe88682dcbb2d7b76682a622a3c6b3cf42b
idfaiz1495 <143871882+idfaiz1495@users.noreply.github.com>
2023-11-11 08:12:37 +1100

    25

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -12,7 +12,9 @@ resource "aws_vpc" "TWF-VPC_Public" {
     tags = {
       Name="TWF-FS"
     }
-  resource "aws_s3_bucket" "s3_bucket"{
+
+resource "aws_s3_bucket" "s3_bucket"{
     bucket="fslab-terraform-1023"
 }
+ 
 }
